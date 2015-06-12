f1945cbfb4b4d823752d2e70a258295a24291621
Baburin Sergey <sbasba777@hotmail.com>
2015-06-12 12:11:12 +0300

    Add parsing terminal commands

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,15 +1,46 @@
 package main;
 
+import main.commands.Command;
+import main.commands.CommandFactory;
+
 public class Parser {
 
     public static Expression parse(String source) {
 
-        Expression result = new Expression();
+        Expression expression = new Expression();
 
         source = source.replaceAll("[^><+-.,\\[\\]]", "");
 
+        int charPointer = 0;
 
+        while (charPointer < source.length()) {
+            expression.add(parse(source.charAt(charPointer++)));
+        }
 
-        return result;
+        return expression;
+    }
+
+    private static Command parse(char c) {
+
+        switch (c) {
+            case Command.NEXT:
+                return CommandFactory.getNext();
+            case Command.PREVIOUS:
+                return CommandFactory.getPrevious();
+            case Command.INCREASE:
+                return CommandFactory.getIncrease();
+            case Command.DECREASE:
+                return CommandFactory.getDecrease();
+            case Command.OUT:
+                return CommandFactory.getOut();
+            case Command.IN:
+                return CommandFactory.getIn();
+            case Command.RIGHT_BRACKET:
+                return CommandFactory.getLoop();
+            case Command.LEFT_BRACKET:
+                return null;
+            default:
+                throw new IllegalArgumentException("Syntax error: unexpected character: " + c);
+        }
     }
 }
